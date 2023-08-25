16fa8166ec1ad868cb66ea79f33f829b1705b700
Trent Fellbootman <2713730481@qq.com>
2023-08-25 21:22:07 +0800

    Revert "attempt to add support for audio with moviepy; inaccurate frame positions in sub-clips are present"
    This reverts commit a734b168ab1dddd58f7d2f0b345c6add831aff2f.

--- utils.py
+++ utils.py
@@ -3,11 +3,6 @@ from typing import Iterable
 from pathlib import Path
 from tqdm import tqdm
 import imageio
-import proglog
-
-proglog.default_bar_logger = lambda *args, **kwargs: proglog.MuteProgressBarLogger()
-
-from moviepy.video.io.VideoFileClip import VideoFileClip
 
 
 def split_video(video_path: Path, output_dir: Path, rtol: float=0.2):
@@ -15,7 +10,6 @@ def split_video(video_path: Path, output_dir: Path, rtol: float=0.2):
     
     The relative difference between two frames, `A`, `B` is calculated as
     `|A - B| / (|A| + |B|)`, which is always between 0 and 1 for real matrices.
-    `|A|` denotes Frobenius norm, i.e., $\sqrt{\sum_{i, j, k} A_{i, j, k}^2}$.
 
     Args:
         video (th.Tensor): The video. Shape should be (frame_index, channel, height, width).
@@ -30,54 +24,54 @@ def split_video(video_path: Path, output_dir: Path, rtol: float=0.2):
     def norm(x: np.ndarray):
         return np.sqrt((x ** 2).sum())
 
-    # inter-frame inconsistency formula implementation
-    def calc_frame_difference(frame1: np.ndarray, frame2: np.ndarray) -> float:
+    def calc_frame_difference(frame1, frame2):
         return norm(frame1 - frame2) / (norm(frame1) + norm(frame2))
 
-    # load the video and get the metadata
-    # use imageio to read each frame as a numpy array
     video_reader = imageio.get_reader(video_path)
     metadata = video_reader.get_meta_data()
-    
-    # use moviepy to create subclips to be written
-    video_clip = VideoFileClip(str(video_path))
-    
-    fps, codec = video_clip.fps, metadata['codec']
+    fps, codec = metadata['fps'], metadata['codec']
 
-    def get_output_path(number: int) -> Path:
-        return Path(output_dir / f"{number}.mp4")
-    
-    # initiate loop-reused variables
+    print(metadata.keys())
+
     last_frame = next(iter(video_reader))
-    start_time = 1 / fps
+
     clip_index = 0
+    def get_output_path(number):
+        return Path(output_dir / f"{number}.mp4")
     
     if not output_dir.is_dir():
         output_dir.mkdir()
         
+    current_writer = imageio.get_writer(get_output_path(clip_index + 1), fps=fps, codec=codec, macro_block_size=1)
+
+    def format_save_message(number, path):
+        return f"Saved clip {number} to {path}."
+    
     n_frames = metadata['duration'] * metadata['fps']
     
     progress = tqdm(video_reader, total=n_frames)
-    for frame_idx, frame in enumerate(progress):
-        progress.set_description(f'clip {clip_index + 1}')
-        
+    for frame in progress:
         diff = calc_frame_difference(last_frame, frame)
         
         assert 0 <= diff < 1
         
         if diff > rtol:
-            # two frames are too inconsistent -> split
-            end_time = (frame_idx + 1) / fps
+            current_writer.close()
+
+            current_path = get_output_path(clip_index + 1)
             
-            sub_clip: VideoFileClip = video_clip.subclip(start_time, end_time)
-            sub_clip.write_videofile(str(get_output_path(clip_index + 1)))
-
             clip_index += 1
-
-            start_time = (frame_idx + 1) / fps
+            
+            current_writer = imageio.get_writer(get_output_path(clip_index + 1), fps=fps, codec=codec, macro_block_size=1)
+            current_writer.append_data(frame)
+            
+            progress.set_description(f'clip {clip_index + 1}')
+        else:
+            current_writer.append_data(frame)
         
         last_frame = frame
     
+    current_writer.close()
     current_path = get_output_path(clip_index + 1)
 
     video_reader.close()
