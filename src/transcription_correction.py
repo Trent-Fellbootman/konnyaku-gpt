b0e1de9aaf88a0fc6ed2f0d205260b96b99fbe9f
Trent Fellbootman <2713730481@qq.com>
2023-08-29 10:29:26 +0800

    added transcription correction utility

--- src/transcription_correction.py
+++ src/transcription_correction.py
@@ -1,6 +1,8 @@
+import sys
 from typing import Collection, Dict, List, Any, Sequence
-from .data_models import ClipData
+import json
 
+from .data_models import ClipData
 from .models.chat_llm import ChatCompletionService
 
 
@@ -10,48 +12,142 @@ class TranscriptionCorrector:
     The intended use is to use one instance of this class for one video.
     """
     
-    def __init__(self, video_background: str, asr_description: str, captioner_description: str, chat_backend: ChatCompletionService):
+    def __init__(self, asr_description: str, captioner_description: str, chat_backend: ChatCompletionService,
+                 max_retry_count: int=3):
         """Constructor.
 
         Args:
-            video_background (str): Background information about the video.
             asr_description (str): Information about the ASR model used to generate the initial transcriptions.
             captioner_description (str): Information about the image captioning model used to generate descriptions for screenshots of the clips.
             chat_backend (ChatCompletionService): A model service for chat-completion which will be used to compute corrected transcriptions.
+            max_retry_count (int): The maximum number to retry on each group of clips. If reached, the clip group will be skipped and no transcription
+                will be generated for this group.
         """
 
-        self.video_background = video_background
         self.asr_description = asr_description
         self.captioner_description = captioner_description
         self.chat_backend = chat_backend
+        self.max_retry_count = max_retry_count
+   
+    def correct_transcriptions(self,
+                               clips_data: Sequence[ClipData],
+                               target_batch_duration: float,
+                               context_pre_batch_duration: float,
+                               context_post_batch_duration: float,
+                               video_background: str) -> List[str | None]:
+        """Correct the transcriptions for a contiguous set of clips.
+
+        Args:
+            clips_data (Sequence[ClipData]): The clips data.
+            target_batch_duration (float): The total length of clips which will be corrected on each LLM operation.
+                Except in boundary occasions, the actual length of those clips is guaranteed to be greater than or equal to this argument.
+            context_pre_batch_duration (float): The total length of context clips before target clips on each LLM operation.
+                Except in boundary occasions, the actual length of those clips is guaranteed to be greater than or equal to this argument.
+            context_post_batch_duration (float): The total length of context clips after target clips on each LLM operation.
+                Except in boundary occasions, the actual length of those clips is guaranteed to be greater than or equal to this argument.
+            video_background (str): Background information about the video.
+
+        Returns:
+            List[str | None]: The corrected transcriptions.
+                None elements mean that the corresponding clips has no speech or were skipped.
+        """
+        
+        current_start = 0
+        transcriptions: List[str] = []
+        
+        while current_start < len(clips_data):
+            # get context_pre
+            context_pre: List[ClipData] = []
+            context_pre_length = 0
+            for i in range(current_start - 1, -1, -1):
+                context_pre.insert(0, clips_data[i])
+                context_pre_length += clips_data[i].duration
+                if context_pre_length >= context_pre_batch_duration:
+                    break
+            
+            # get target clips
+            target_clips: List[clips_data] = []
+            target_clips_length = 0
+            for i in range(current_start, len(clips_data)):
+                target_clips.append(clips_data[i])
+                target_clips_length += clips_data[i].duration
+                if target_clips_length >= target_batch_duration:
+                    break
+            
+            # get context_post
+            context_post: List[ClipData] = []
+            context_post_length = 0
+            for i in range(current_start + 1, len(clips_data)):
+                context_post.append(clips_data[i])
+                context_post_length += clips_data[i].duration
+                if context_post_length >= context_post_batch_duration:
+                    break
+            
+            # correct
+            result = self._correct_clip_transcriptions(context_pre, target_clips, context_post, video_background)
+            
+            # update
+            if result is not None:
+                for i in range(len(target_clips)):
+                    transcriptions.append(result.get(i, None))
+            else:
+                transcriptions += [None] * len(target_clips)
+                print(f'Error occurred when correcting transcriptions for clips [{current_start}, {current_start + len(target_clips)})', file=sys.stderr)
+                
+            current_start += len(target_clips)
+
+            print(f'\rFinished clips {current_start + len(target_clips)}/{len(clips_data)}')
+            
+            assert current_start + len(target_clips) == len(transcriptions)
+        
+        return transcriptions
     
-    def correct_transcriptions(self, context_pre: Sequence[ClipData], targets: Sequence[ClipData], context_post: Sequence[ClipData]) -> Sequence[str]:
+    def _correct_clip_transcriptions(self, context_pre: Sequence[ClipData], targets: Sequence[ClipData], context_post: Sequence[ClipData], video_background: str) -> Dict[int, str] | None:
         """Corrects the transcriptions of the clips in `targets`.
 
         Args:
             context_pre (Sequence[ClipData]): Data about the clips before `targets`.
             targets (Sequence[ClipData]): Data about the clips to be transcribed.
             context_post (Sequence[ClipData]): Data about the clips after `targets.
+            video_background (str): Background information about the video.
 
         Returns:
-            Sequence[str]: The corrected transcriptions.
-                Each element corresonponds to a clip in `targets`.
+            Dict[int, str] | None: The corrected transcriptions.
+                The keys correspond to clip indices in `targets`, while the values are the corresponding transcriptions.
+                Returning None means there is max_retry_count exceeded.
         """
         
-        correction_prompt = self._build_transcription_correction_prompt(context_pre, targets, context_post)
-        correction_output: str = self.chat_backend([(correction_prompt, True)])
-        formatting_prompt = self._build_transcription_formatting_prompt(correction_output, len(targets))
-        formatting_output: str = self.chat_backend([formatting_prompt])
+        result = None
         
-        transcriptions = formatting_output.split('\n' * 2)
+        for _ in range(self.max_retry_count):
+            try:
+                correction_prompt = self._build_transcription_correction_prompt(context_pre, targets, context_post, video_background)
+                correction_output: str = self.chat_backend([(correction_prompt, True)])
+                formatting_prompt = self._build_transcription_formatting_prompt(correction_output, len(targets))
+                formatting_output: str = self.chat_backend([formatting_prompt])
+                
+                transcriptions: Dict[str, str] = json.loads(formatting_output)
+                
+                target_clip_indices = {len(context_pre) + i for i in range(len(targets))}
+                
+                if (not target_clip_indices.issuperset(int(key) for key in transcriptions.keys())) \
+                    or (not all(isinstance(val, str) for val in transcriptions.values())):
+                    raise Exception("Error occurred in transcription correction.")
 
-        return transcriptions
+                result = {int(key) - len(context_pre): transcriptions[key] for key in transcriptions.keys()}
+                
+                break
+            except Exception:
+                continue
+        
+        return result
 
     def _build_transcription_correction_prompt(
         self,
         context_pre: Sequence[ClipData],
         targets: Sequence[ClipData],
-        context_post: Sequence[ClipData]) -> str:
+        context_post: Sequence[ClipData],
+        video_background: str) -> str:
         """Builds the prompt used to be fed into an LLM and retrieve corrected transcriptions.
         
         The output of the LLM when fed with the returned value is expected to contain the corrected transcriptions,
@@ -61,6 +157,7 @@ class TranscriptionCorrector:
             context_pre (Sequence[ClipData]): Data about the clips before `targets`.
             targets (Sequence[ClipData]): Data about the clips to be transcribed.
             context_post (Sequence[ClipData]): Data about the clips after `targets.
+            video_background (str): Background information about the video.
 
         Returns:
             str: The prompt ready to be fed into an LLM.
@@ -93,6 +190,11 @@ As a result, it is usual for one clip to contain multiple sentences spoken by mu
 or just an incomplete part of one sentence spoken by one speaker.
 Hence, you should NOT assume that each clip always contains exactly one complete sentence.
 If you think a clip only contains part of a sentence, you should also provide part of the corrected transcription accordingly.
+Additionally, some clips may not contain any speech at all
+(it is also possible that the ASR output is not empty even though the clip contains no speech, so use your discretion).
+
+If you think that a clip does not contain any speech or you really cannot infer a plausible transcription (e.g., when the ASR output is nonsense),
+just omit that clip in your output.
 
 To be more specific, here is a description of the ASR model:
 
@@ -117,7 +219,7 @@ For your reference, here is a description of the image-to-text model:
 
 Also, you may find the following background information of the video helpful:
 
-{self.video_background}
+{video_background}
 
 Now, here are the information of the clips (0 indexed, indices correspond to the order in which the clips appears in the video):
 
@@ -135,7 +237,7 @@ and give me the audio transcriptions that seem most REASONABLE and PLAUSIBLE to 
 
 You should provide YOUR transcriptions for clip {len(context_pre)}-{len(context_pre) + len(targets) - 1} ONLY.
 DO NOT include ASR outputs, image-to-text outputs, or information about contextual clips.
-However, please do indicate which clip each of your transcription corresponds to.
+However, please DO indicate the index of the clip that each of your transcriptions corresponds to.
 """
 
 
@@ -152,40 +254,36 @@ However, please do indicate which clip each of your transcription corresponds to
         
         return \
 f"""I used an LLM to correct transcriptions for a number of audio clips.
-Please format the LLM output so that I can use some simple code to retrieve the transcriptions in a machine-friendly form.
+Please format the LLM output into JSON so that I can use some simple code to retrieve the transcriptions in a machine-friendly form.
 
 The transcriptions in the LLM output are ordered by clip indices (not necessarily starting from 0 or 1),
-and I want you to output the transcription CONTENT of each clip IN ORDER (lower indices first),
-leaving a SINGLE blank lines between transcriptions of each two consecutive clips
-(If a transcription of a certain clip already contains blank lines, remove them to avoid ambiguity).
+and I want you to output the a JSON dictionary where the keys are clip indices and the values are the corresponding transcriptions.
+If the transcription of a clip is an empty string, you may either include it (setting the corresponding value to "") in your JSON output or omit it.
 
 For example, if the LLM output is:
 
     Sure! Here are my transcriptions for clip 15-17:
     
-    Clip 15:
+    Clip 15 transcription:
     blah1 blah1 blah1
     
-    Clip 17:
+    Clip 110:
     blah2
     
     blah2
     blah2
     
     Clip 16:
-    blah3 blah3 blah3
     
-Then your output should be (do not actually contain the indentations. They are for pretty-printing purposes only):
+Then your output should be something like:
 
-    blah1 blah1 blah1
-    
-    blah3 blah3 blah3
-    
-    blah2
-    blah2
-    blah2
+    {{
+        "15": "blah1 blah1 blah1",
+        "16": "",
+        "110": "blah2\\n\\nblah2\\nblah2"
+    }}
 
-Notice that "blah3" actually appears before "blah2", because "blah3" corresponds to clip 16, while "blah2" corresponds to clip 17.
+Notice that the clip indices MAY NOT be a list of contiguous numbers.
 
 Now, the LLM output I got is:
 
--- test.py
+++ test.py
@@ -1,16 +1,6 @@
-from src.data_models import ClipData, ClipsMetadata
-
-
-a = ClipData(video_path='test', audio_transcription_raw='test', screenshot_description='test')
-
-print(a.to_json())
-
-assert a == ClipData.from_json(a.to_json())
-
-b = ClipsMetadata(
-    ['1.mp4', '2.mp4', '4.mp4']
-)
-
-print(b.to_json())
-
-assert b == ClipsMetadata.from_json(b.to_json())
+for i in range(100):
+    try:
+        print(i)
+        break
+    except Exception:
+        continue
